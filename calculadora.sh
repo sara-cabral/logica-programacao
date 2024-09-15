6300e8ad966cbf4b80763fea31331bf90f2acecd
sara-cabral <saracabralmenezes@hotmail.com>
2024-09-14 21:52:38 -0300

    Create calculadora.sh
    Create file calculadora.sh

---
+++ calculadora.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Calculadora simples"
+echo "Digite o número de visitantes"
+read visitantes
+echo "Digite o número de conversões"
+read conversoes
+echo "Escolha a operação (+, -, *, /):"
+read op
+
